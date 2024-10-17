eacb48498a67c100470c3667b06e28fcdc041da6
Cody Weaver <codyleeweaver@gmail.com>
2024-10-16 21:42:15 -0700

    Initial Commit

---
+++ WorkoutShare/WorkoutShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutShareApp.swift
+//  WorkoutShare
+//
+//  Created by Cody Weaver on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WorkoutShareTests/WorkoutShareTests.swift
@@ -0,0 +1,16 @@
+//
+//  WorkoutShareTests.swift
+//  WorkoutShareTests
+//
+//  Created by Cody Weaver on 10/16/24.
+//
+
+import Testing
+
+struct WorkoutShareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
